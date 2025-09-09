ddec7c4e162d2a1cd710864c2d6c9015c87cdeea
mynamejason <ykim4141@gmail.com>
2025-09-09 12:07:33 -0400

    Started machine learning

---
+++ python/machine_learning/machine_learning.py
@@ -0,0 +1,45 @@
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)\
+
+#print(dataset.shape)                                        # 150 Instances, 5 Attributes
+
+#print(dataset.head(20))                                     # Check the first 20 rows of data
+
+#print(dataset.describe())                                   # Sees the overall summary of each attributes
+
+#print(dataset.groupby('class').size())                      # Class distribution
+
+
+# The following won't show up without proper visualization support, such as Anaconda (I think):
+#---
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+#---
+
